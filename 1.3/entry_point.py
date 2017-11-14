892bea58fe36170d0e22ae4bd44bb62ac2018c63
Seungbae Ji <seungbae.ji@gmail.com>
2017-11-14 19:21:40 +0900

    Entry point is add

---
+++ 1.3/entry_point.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import model
+
+
+
+
+
+if __name__ == "__main__":
+    boxes = [model.Box("red"), model.Box("blue"), model.Box("green")]
